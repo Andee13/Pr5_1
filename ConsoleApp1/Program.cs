f8f6c1c6c1decca9d09f0507d0835f0bfa297004
Andee13 <39676479+Andee13@users.noreply.github.com>
2019-03-30 12:37:09 +0200

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class My
+    {
+        private int a = 5 ;
+        private int b = 4;
+
+        
+        public int C
+        {
+            get { return a + b; }
+        }
+
+        
+    }
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            My program = new My();
+            Console.WriteLine("" + program.C);
+            
+        }
+    }
+}
